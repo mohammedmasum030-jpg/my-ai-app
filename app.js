a0b2c49fc9320b475b079ddeae03003651cd6a25
mohammedmasum030-jpg <mohammedmasum030@gmail.com>
2026-05-24 22:01:52 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
+const API_KEY = "AIzaSyA6GDfRNIE92CsjqMWvQdh5-vES4D_IX2k ";
+
 async function send() {
+
   let input = document.getElementById("input").value;
-  let chat = document.getElementById("chat");
 
-  chat.innerHTML += "You: " + input + "<br>";
-
-  const response = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=YOUR_API_KEY", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      contents: [{ parts: [{ text: input }] }]
-    })
-  });
+  const response = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [
+              {
+                text: input
+              }
+            ]
+          }
+        ]
+      })
+    }
+  );
 
   const data = await response.json();
 
-  let reply = data.candidates[0].content.parts[0].text;
+  console.log(data);
 
-  chat.innerHTML += "AI: " + reply + "<br>";
+  let reply =
+    data.candidates[0].content.parts[0].text;
+
+  document.getElementById("chat").innerHTML +=
+    "<br>AI: " + reply;
 }
